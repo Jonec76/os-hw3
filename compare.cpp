8c0244f48544af4af849fa7d2b7ba56c9c59b37a
jonec76 <jonec9676@gmail.com>
2020-12-09 15:28:54 +0800

    Modify the compare code

--- compare.cpp
+++ compare.cpp
@@ -1,26 +1,67 @@
-#include <fstream>
-#include <iostream>
-#include <stdio.h>
-#include <string.h>
+// C program to compare two files and report 
+// mismatches by displaying line number and 
+// position of line. 
+#include<stdio.h> 
+#include<string.h> 
+#include<stdlib.h> 
 
-using namespace std;
+void compareFiles(FILE *fp1, FILE *fp2) 
+{ 
+	// fetching character of two file 
+	// in two variable ch1 and ch2 
+	char ch1 = getc(fp1); 
+	char ch2 = getc(fp2); 
 
-int main() {
-    fstream file1("hw3example.output"), file2("test.output");
-    char string1[256], string2[256];
-    int j;
-    j = 0;
+	// error keeps track of number of errors 
+	// pos keeps track of position of errors 
+	// line keeps track of error line 
+	int error = 0, pos = 0, line = 1; 
 
-    while (!file1.eof()) {
-        file1.getline(string1, 256);
-        file2.getline(string2, 256);
-        j++;
-        if (strcmp(string1, string2) != 0) {
-            cout << j << "-th strings are not equal"
-                 << "\n";
-            cout << "   " << string1 << "\n";
-            cout << "   " << string2 << "\n";
-        }
-    }
-    return 0;
-}
+	// iterate loop till end of file 
+	while (ch1 != EOF && ch2 != EOF) 
+	{ 
+		pos++; 
+
+		// if both variable encounters new 
+		// line then line variable is incremented 
+		// and pos variable is set to 0 
+		if (ch1 == '\n' && ch2 == '\n') 
+		{ 
+			line++; 
+			pos = 0; 
+		} 
+
+		// if fetched data is not equal then 
+		// error is incremented 
+		if (ch1 != ch2) 
+		{ 
+			error++; 
+			printf("Line Number : %d \tError"
+			" Position : %d \n", line, pos); 
+		} 
+
+		// fetching character until end of file 
+		ch1 = getc(fp1); 
+		ch2 = getc(fp2); 
+	} 
+
+	printf("Total Errors : %d\t", error); 
+} 
+
+// Driver code 
+int main() 
+{ 
+	// opening both file in read only mode 
+	FILE *fp1 = fopen("2.output", "r"); 
+	FILE *fp2 = fopen("result.output", "r"); 
+
+	if (fp1 == NULL || fp2 == NULL) 
+	{ 
+        printf("Error : Files not open"); 
+        exit(0); 
+	} 
+
+
+
+	return 0; 
+} 
